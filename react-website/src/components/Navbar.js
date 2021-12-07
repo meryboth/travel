60379c48cc8aad464386053b4412796dba542416
Marilyn Botheatoz <65403789+meryboth@users.noreply.github.com>
2021-12-07 16:22:27 -0300

    configuracion inicial y componente nav

---
+++ react-website/src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React, {useState} from 'react'
+import { Link } from 'react-router-dom'
+
+function Navbar() {
+    return (
+        <>
+        <nav className="navbar">
+            <div className="navbar-container">
+            <Link to="/" className="navbar-logo">
+               TVRL
+               <i className="fab fa-typo3"></i>
+            </Link>
+            </div>
+        </nav>
+        </>
+    )
+}
+
+export default Navbar
